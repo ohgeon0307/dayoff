1130b250a64f4eb896feae0c5bdf8a736cb19966
708 <708@DESKTOP-4FLJFEG>
2023-01-11 16:13:31 +0900

    커밋테스트

--- SpringMVC/java/edu/study/controller/PhotoController.java
+++ SpringMVC/java/edu/study/controller/PhotoController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "/photo")
-//가상경로 찾는 어노테이션
+//가상경로 찾는 어노테이션입니다.
 @Controller
 public class PhotoController {
 
