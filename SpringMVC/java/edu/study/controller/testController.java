70ec42584e11ad50af952be3d7e04fc122c11fde
15351 <15351@DESKTOP-3RHRVJD>
2023-01-11 16:27:34 +0900

    1234

--- SpringMVC/java/edu/study/controller/testController.java
+++ SpringMVC/java/edu/study/controller/testController.java
@@ -1,9 +1,15 @@
 package edu.study.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "/test")
-public class testController {
 
+@Controller
+public class testController {
+	public String test() {
+		System.out.println("hi");
+		return "";
+	}
 		
 }
